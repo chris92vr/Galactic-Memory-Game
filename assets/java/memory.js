26c2df6dcd7d66df87d95b5e1da258bff6371273
chris92vr <garofolichristian@gmail.com>
2020-08-06 18:33:09 +0000

    Added createStartButton method

--- assets/java/memory.js
+++ assets/java/memory.js
@@ -22,4 +22,14 @@ class GalacticMemoryGame{
         this.settings=Object.assign({},this.settings);
         this.createButtonStart();
     }
+    createStartButton(){
+        this.containe=$(this.container);
+        var _this=this;
+        this.container.html('<button class="start id="start"'+_this.settings.uniqueId+'">Start</button>');
+        $('#start'+_this.settings.uniqueId).click(function(){
+            this.container.html("");
+            this.createGame();
+        });
+    }
+    
 }
