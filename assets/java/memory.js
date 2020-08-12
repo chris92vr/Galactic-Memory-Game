d308325ce9ad9ca6754df8650b376d7a2f3c9ae9
chris92vr <garofolichristian@gmail.com>
2020-08-12 08:13:33 +0000

    Fixed typos and fixed restart button alignment problem

--- assets/java/memory.js
+++ assets/java/memory.js
@@ -1,151 +1,182 @@
-$(document).ready(function(){
-var deck = new GalacticMemory();
+$(document).ready(function() {
+    var deck = new GalacticMemoryGame();
 });
- 
-class GalacticMemoryGame{
-    constructor(settings){
-        this.settings= {
-            container = ".memory",
-            planets: [
-                {name:"earth",img:"img/earth.jpg"},
-                {name:"europa",img:"img/europa.jpg"},
-                {name:"jupiter",img:"img/jupiter.jpg"},
-                {name:"mars",img:"img/mars.jpg"},
-                {name:"mercury",img:"img/mercury1.png"},
-                {name:"moon",img:"img/moon.jpg"},
-                {name:"neptune",img:"img/neptune.jpg"},
-                {name:"uranus",img:"img/uranus.jpg"},
-                {name:"plutos",img:"img/pluto.jpg"},
-                {name:"titan",img:"img/Titan.jpg"},
-                {name:"venus",img:"img/venus.jpg"},
-                {name:"saturn",img:"img/saturn.jpg"}
+
+class GalacticMemoryGame {
+    constructor(settings) {
+        this.settings = {
+            container : '.memory',
+            planets: [{
+                    name: "earth",
+                    img: "img/earth.jpg"
+                },
+                {
+                    name: "europa",
+                    img: "img/europa.jpg"
+                },
+                {
+                    name: "jupiter",
+                    img: "img/jupiter.jpg"
+                },
+                {
+                    name: "mars",
+                    img: "img/mars.jpg"
+                },
+                {
+                    name: "mercury",
+                    img: "img/mercury1.png"
+                },
+                {
+                    name: "moon",
+                    img: "img/moon.jpg"
+                },
+                {
+                    name: "neptune",
+                    img: "img/neptune.jpg"
+                },
+                {
+                    name: "uranus",
+                    img: "img/uranus.jpg"
+                },
+                {
+                    name: "plutos",
+                    img: "img/pluto.jpg"
+                },
+                {
+                    name: "titan",
+                    img: "img/Titan.jpg"
+                },
+                {
+                    name: "venus",
+                    img: "img/venus.jpg"
+                },
+                {
+                    name: "saturn",
+                    img: "img/saturn.jpg"
+                }
             ],
-            uniqueId:"c0d31nst1tut3"
+            uniqueId: 'c0d31nst1tut3'
         }
 
-        this.settings=Object.assign({},this.settings);
+        this.settings = Object.assign({}, this.settings, settings);
         this.createButtonStart();
     }
-    createStartButton(){
-        this.containe=$(this.container);
-        var _this=this;
-        this.container.html('<button class="start id="start"'+_this.settings.uniqueId+'">Start</button>');
-        $('#start'+_this.settings.uniqueId).click(function(){
-            this.container.html("");
-            this.createGame();
+    createButtonStart() {
+        this.container = $(this.settings.container);
+        var _this = this;
+        this.container.html('<button class="start" id="start' + _this.settings.uniqueId + '">Start</button>');
+        $('#start' + _this.settings.uniqueId).click(function() {
+        _this.container.html('');
+        _this.createGame();
         });
-    }
-    createGame(){
-        this.deck =this.settings.planets.concat(this.settings.planets).sort(()=>0.5 -Math.random());
-        this.matched =0;
-        this.actualCard="";
-        this.score= 0;
-        this.isSTart=false;
-        this.cardsFounded=0;
+     }
+   
+    createGame() {
+        this.deck = this.settings.planets.concat(this.settings.planets).sort(() => 0.5 - Math.random());
+        this.matched = 0;
+        this.actualCard = "";
+        this.score = 0;
+        this.isStart = false;
+        this.cardsFounded = 0;
         this.createMenu();
-        for(var i=0;i<24;i++) this.createCard(i);
-        this.showAllCards;
+        for (var i = 0; i < 24; i++) this.createCard(i);
+        this.showAllCards();
     }
-    createMenu(){
-        var _this=this;
-        this.containe.append('<div class="menu">'+'Score:<span id="score'+this.settings.uniqueId+'">0</span></div> &ensp;&ensp;&ensp;&ensp;<span id="restart'+this.settings.uniqueId+'"class="restart">Restart</span>&ensp;&ensp;&ensp;</div>');
-        $('#restart'+this.settings.uniqueId).click(function(){
+    createMenu() {
+        var _this = this;
+        this.container.append('<div class="menu">' + 'Score:<span id="score' + this.settings.uniqueId + '">0</span> &ensp;&ensp;&ensp;<span id="restart' + this.settings.uniqueId + '" class="restart">Restart</span></div>');
+        $('#restart' + this.settings.uniqueId).click(function() {
             _this.container.html('');
             _this.createGame();
         });
     }
-    showAllCards(){
-        var _this=this;
+    showAllCards() {
+        var _this = this;
         setTimeout(
-            function(){
+            function() {
                 $('.card').flip(true);
                 setTimeout(
-                    function(){
+                    function() {
                         $('.card').flip(false);
-                        _this.isStart= true;
-                    },3000);
+                        _this.isStart = true;
+                    }, 3000);
             }, 500);
-        }
-    endGame(){
-        var _this =this;
-        if(this.cardsFounded>0){
-        this.container.html('div class="end-message"><h3>You Win!</h3><br>You totaled'+this.cardsFounded.toLocaleString()+'points</div>');
-        }else this.container.html('<div class="end-message"><h3>You Lose!</h3><br>Try Again</div><div class="playAgain" id="playAgain'+_this.settings.uniqueId+'">play again</div>');
-        $('#playAgain'+this.settings.uniqueId).click(function(){
-        _this.container.html('');
-        _this.createGame();
-    });
+    }
+    endGame() {
+        var _this = this;
+        if (this.cardsFounded > 0) {
+            this.container.html('div class="end-message"><h3>You Win!</h3><br>You totaled' + this.cardsFounded.toLocaleString() + 'points</div>');
+        } else this.container.html('<div class="end-message"><h3>You Lose!</h3><br>Try Again</div><div class="playAgain" id="playAgain' + _this.settings.uniqueId + '">play again</div>');
+        $('#playAgain' + this.settings.uniqueId).click(function() {
+            _this.container.html('');
+            _this.createGame();
+        });
 
-}
-incScore(){
-    $('#score'+this.settings.uniqueId).text(this.cardsFounded);
-    if(((this.cardsFounded)+((24-((this.matched)*2))*5))>0){
-        this.endGame();
+    }
+    incScore() {
+        $('#score' + this.settings.uniqueId).text(this.cardsFounded);
+        if (((this.cardsFounded) + ((24 - ((this.matched) * 2)) * 5)) < 0) {
+            this.endGame();
         }
     }
-createCard(id){
-    var front = '<div class="front"></div>';
-    var back = '<div class="back"><div class="title">'+this.deck[id].name+'</div></div>';
-    var _this=this;
-    this.container.append('<div id="card'+id+'-'+this.settings.uniqueId+'"class="card" data-name="'+this.deck[id].name+'"data-playable="1">'+front+back+'</div>');
-    $('#card'+id+'-'+this.settings.uniqueId+'.back').css('background-image','url("'+this.deck[id].img+'")');
-     $('#card'+id+'-'+this.settings.uniqueId).flip({
-         axis:yield,
-         trigger:'manual'
-     });
-      $('#card'+id+'-'+this.settings.uniqueId).click(function(){
-        if($(this).attr('data-playable')== 1 && _this.isStart){
-            var card=$(this.data("flip-model"));
-            if(card.isFlipped){
-                $(this).flip(false);
-            }else{
-                $(this).flip(true);
+    createCard(id) {
+        var front = '<div class="front"></div>';
+        var back = '<div class="back"><div class="title">' + this.deck[id].name + '</div></div>';
+        var _this = this;
+        this.container.append('<div id="card' + id + '-' + this.settings.uniqueId + '"class="card" data-name="' + this.deck[id].name + '" data-playable="1">' + front + back + '</div>');
+        $('#card' + id + '-' + this.settings.uniqueId + '.back').css('background-image', 'url("' + this.deck[id].img + '")');
+        $('#card' + id + '-' + this.settings.uniqueId).flip({
+            axis: 'y',
+            trigger: 'manual'
+        });
+        $('#card' + id + '-' + this.settings.uniqueId).click(function() {
+            if ($(this).attr('data-playable') == 1 && _this.isStart) {
+                var card = $(this).data("flip-model");
+                if (card.isFlipped) {
+                    $(this).flip(false);
+                } else {
+                    $(this).flip(true);
+                }
+                var playedCard = $(this).attr('data-name');
+                if (_this.actualCard == "") {
+                    _this.actualCard = playedCard;
+                    $(this).attr('data-playable', 0);
+                } else {
+                    if (_this.actualCard == playedCard) {
+                        // correct
+
+                        $('div[data-name="' + _this.actualCard + '"]').attr('data-playable', 0);
+
+                        _this.matched++;
+                        _this.cardsFounded += 10;
+
+                        _this.incScore();
+                        console.log(_this.matched);
+                        if (_this.matched == 12) {
+                            _this.endGame();
+                        }
+                    } else if (_this.actualCard != "" && _this.actualCard != playedCard) {
+                        // wrong
+                        _this.cardsFounded -= 2;
+                        _this.incScore();
+                        var actualCard = _this.actualCard;
+
+                        setTimeout(
+                            function() {
+
+                                $('div[data-name="' + actualCard + '"]').flip(false);
+                                $('div[data-name="' + playedCard + '"]').flip(false);
+                                $('div[data-name="' + actualCard + '"]').attr('data-playable', 1);
+                                $('div[data-name="' + playedCard + '"]').attr('data-playable', 1);
+
+                            }, 1000);
+
+                    }
+                    _this.actualCard = "";
+                }
             }
-            var playedCard=$(this).attr('data-name');
-           if( _this.actualCard == "" ) {
-	_this.actualCard = playedCard;
-	$(this).attr('data-playable',0);
-	} else {
-	if( _this.actualCard == playedCard ) {
-    // correct
-  
-	$('div[data-name="'+_this.actualCard+'"]').attr('data-playable',0);
-   
-    _this.matched++;
-    _this.cardsFounded+=10;
-        
-    _this.incScore();
-   console.log(_this.matched);
-    if (_this.matched == 12){
-        _this.endGame();
-    }
-   } else if( _this.actualCard != "" && _this.actualCard != playedCard ) {
-	// wrong
-    _this.cardsFounded-=2;
-    _this.incScore();
-    var actualCard = _this.actualCard;
 
-	setTimeout(
-	function(){
-    
-	$('div[data-name="'+actualCard+'"]').flip(false);
-	$('div[data-name="'+playedCard+'"]').flip(false);
-	$('div[data-name="'+actualCard+'"]').attr('data-playable',1);
-    $('div[data-name="'+playedCard+'"]').attr('data-playable',1);
-   
-	}, 1000);
-	
-	}
-	_this.actualCard = "";
-	}
-    }
-    
-	});
-    
+        });
+
     }
 }
-        
-        
-    
-
-    
